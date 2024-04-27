8088ad3bb46e7e4839778ab98f62512c48b9e782
shubham74817 <103422128+shubham74817@users.noreply.github.com>
2024-04-27 12:26:14 +0000

    Conversion in datatypes.

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+// const score=false
+
+// // console.log(typeof score);
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedIn="xyz";
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber=33
+let someNumberString=String(someNumber)
+console.log(typeof someNumberString)
